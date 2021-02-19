28c58359e515292abca87c343271a1edf3096c21
Mohamed Irfan <irfancuber@gmail.com>
2021-02-19 10:29:45 +0800

    Added endpoints for signin & login in app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+
+const signin = require('./controllers/signin');
+const register = require('./controllers/register');
+
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
 // const { Client } = require("pg");
 // const client = new Client();
@@ -9,5 +18,10 @@ app.get("/", (req, res, next) => {
   // console.log("hello world");
   res.send("Hello from backend");
 });
+
+// Sign in route for intructor.
+// First arg is database reference, second is bcrypt to hash password
+app.post('/signin', signin.signinHandler(db, bcrypt))
+app.post('/register', register.registerHandler(db, bcrypt))
 
 module.exports = app;
