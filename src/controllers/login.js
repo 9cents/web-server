d22d6681fc035d846ec9de9144b4a105006385d9
Damien Cheng <47481731+DemonicMushy@users.noreply.github.com>
2021-02-27 03:09:19 +0800

    Added JWT functionality for login

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,33 +1,36 @@
 const loginHandler = (db, bcrypt) => (req, res) => {
   const query = {
     name: req.query.name,
-    password: req.query.password
-  }
+    password: req.query.password,
+  };
 
   if (!query.name || !query.password) {
-    return res.status(500).json({message: 'Entries must not be empty!'});
+    return res.status(500).json({ message: "Entries must not be empty!" });
   }
 
-  var queryText = "SELECT * FROM player WHERE player_name = '"
-  + query.name + "'";
+  var queryText =
+    "SELECT * FROM player WHERE player_name = '" + query.name + "'";
 
   // Database SQL query goes here
   db.query(queryText, (err, response) => {
     if (err) {
-      res.status(500).json({message: err});
+      res.status(500).json({ message: err });
     } else {
-      if (response.rows[0]){
-        bcrypt.compare(query.password, response.rows[0].password, function(err, isMatch) {
-          if(!isMatch){
-            res.status(401).json({message: 'Passwords do not match'});
-          } else {
-            res.status(200).json({message: 'Passwords match'});
-            // Send JWT
+      if (response.rows[0]) {
+        bcrypt.compare(
+          query.password,
+          response.rows[0].password,
+          function (err, isMatch) {
+            if (!isMatch) {
+              res.status(401).json({ message: "Passwords do not match" });
+            } else {
+              res.status(200).json({ message: "Passwords match" });
+              // Send JWT (the game doesn't need I think)
+            }
           }
-        });
-      }
-      else {
-        res.status(401).json({message: 'Player not found.'});
+        );
+      } else {
+        res.status(401).json({ message: "Player not found." });
       }
     }
   });
@@ -36,33 +39,40 @@ const loginHandler = (db, bcrypt) => (req, res) => {
 const loginHandlerWeb = (db, bcrypt) => (req, res) => {
   const query = {
     name: req.query.name,
-    password: req.query.password
-  }
+    password: req.query.password,
+  };
 
   if (!query.name || !query.password) {
-    return res.status(500).json({message: 'Entries must not be empty!'});
+    return res.status(500).json({ message: "Entries must not be empty!" });
   }
 
-  var queryText = "SELECT * FROM instructor WHERE instructor_name = '"
-  + query.name + "'";
+  var queryText =
+    "SELECT * FROM instructor WHERE instructor_name = '" + query.name + "'";
 
   // Database SQL query goes here
   db.query(queryText, (err, response) => {
     if (err) {
-      res.status(500).json({message: err});
+      res.status(500).json({ message: err });
     } else {
-      if (response.rows[0]){
-        bcrypt.compare(query.password, response.rows[0].password, function(err, isMatch) {
-          if(!isMatch){
-            res.status(401).json({message: 'Passwords do not match'});
-          } else {
-            res.status(200).json({message: 'Passwords match'});
-            // Send JWT
+      if (response.rows[0]) {
+        bcrypt.compare(
+          query.password,
+          response.rows[0].password,
+          function (err, isMatch) {
+            if (!isMatch) {
+              res.status(401).json({ message: "Passwords do not match" });
+            } else {
+              const payload = {
+                sub: query.name,
+              };
+              // proper implementation will not hardcode secret
+              const token = jwt.sign(payload, "supersecretstring");
+              res.status(200).json({ message: "Passwords match", jwt: token });
+            }
           }
-        });
-      }
-      else {
-        res.status(401).json({message: 'Instructor not found.'});
+        );
+      } else {
+        res.status(401).json({ message: "Instructor not found." });
       }
     }
   });
@@ -70,5 +80,5 @@ const loginHandlerWeb = (db, bcrypt) => (req, res) => {
 
 module.exports = {
   loginHandler,
-  loginHandlerWeb
+  loginHandlerWeb,
 };
